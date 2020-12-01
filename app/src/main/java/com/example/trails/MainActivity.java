9aa7b49d7d32ab7b978c505be1ac13e55f5a8001
JoaoLopes97 <joaodiogolopes97@hotmail..com>
2020-12-01 21:35:02 +0000

    explore fragment missing hide on scroll

--- app/src/main/java/com/example/trails/MainActivity.java
+++ app/src/main/java/com/example/trails/MainActivity.java
@@ -6,7 +6,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,18 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_explore, R.id.nav_my_trails, R.id.nav_start, R.id.nav_profile, R.id.nav_settings)
-                .build();
-
 
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager()
                         .findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        getSupportActionBar().setElevation(0);
     }
 }
---
+++ app/src/main/java/com/example/trails/ui/explore/RecyclerViewAdapter.java
@@ -0,0 +1,71 @@
+package com.example.trails.ui.explore;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.trails.R;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolderNew> {
+
+    Context context;
+    private ArrayList<TrailCard> trails;
+
+    public RecyclerViewAdapter(Context context, ArrayList<TrailCard> trails) {
+        this.context = context;
+        this.trails = trails;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderNew onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view, parent, false);
+
+        return  new ViewHolderNew(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderNew holder, int position) {
+        TrailCard trailCard = trails.get(position);
+        holder.setDetails(trailCard);
+    }
+
+    @Override
+    public int getItemCount() {
+        return trails.size();
+    }
+
+    class ViewHolderNew extends RecyclerView.ViewHolder {
+
+        TextView txtTrailNameCard,txtLocationCard,txtReviewsCard;
+        RatingBar txtRatingCard;
+        ImageView trailPhotoCard;
+
+        public ViewHolderNew(@NonNull View itemView) {
+            super(itemView);
+            txtTrailNameCard = itemView.findViewById(R.id.trail_name);
+            txtLocationCard = itemView.findViewById(R.id.trail_location);
+            txtRatingCard = itemView.findViewById(R.id.trail_rating);
+            txtReviewsCard = itemView.findViewById(R.id.trail_reviews);
+            trailPhotoCard = itemView.findViewById(R.id.trail_photo);
+        }
+
+        void setDetails(TrailCard trail){
+            txtTrailNameCard.setText(trail.getTrailName());
+            txtLocationCard.setText(trail.getLocation());
+            txtRatingCard.setRating(trail.getRating());
+            txtReviewsCard.setText(trail.getReviews() + " Reviews");
+            trailPhotoCard.setImageResource(trail.getPhoto());
+        }
+    }
+}
