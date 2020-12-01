354e212241af1768ed49485a2f60b136722b19c1
barbara <barb.97@hotmail.com>
2020-12-01 01:33:57 +0000

    explorer screen - search bar, filter button and view mode button

--- app/src/main/java/com/example/trails/ui/explore/ExploreFragment.java
+++ app/src/main/java/com/example/trails/ui/explore/ExploreFragment.java
@@ -13,10 +13,13 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.trails.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ExploreFragment extends Fragment {
 
     private ExploreViewModel exploreViewModel;
+    private FloatingActionButton viewMode;
+    private boolean viewMode_Map = true; // true if viewMode is Map, false if viewMode is List.
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +33,24 @@ public class ExploreFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        viewMode = root.findViewById(R.id.view_mode);
+        viewMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeViewMode();
+            }
+        });
         return root;
+    }
+
+    private void changeViewMode(){
+        if(viewMode_Map){
+            viewMode.setImageResource(R.drawable.ic_baseline_list_24);
+            viewMode_Map = false;
+        }else {
+            viewMode.setImageResource(R.drawable.ic_baseline_map_24);
+            viewMode_Map = true;
+        }
     }
 }
