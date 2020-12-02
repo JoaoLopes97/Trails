c99f174e8cc097322b85bb7ce3f417936c471d69
JoaoLopes97 <joaodiogolopes97@hotmail..com>
2020-12-02 23:26:28 +0000

    explore fragment filter

--- app/src/main/java/com/example/trails/ui/explore/ExploreFragment.java
+++ app/src/main/java/com/example/trails/ui/explore/ExploreFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.example.trails.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,20 +20,39 @@ public class ExploreFragment extends Fragment {
     private FloatingActionButton viewMode;
     private ListFragment listFragment;
     private MapFragment mapFragment;
+    private FilterFragment filterFragment;
     public static ArrayList<TrailCard> trailCards = new ArrayList<>();
 
     private boolean viewMode_Map = false; // true if viewMode is Map, false if viewMode is List.
+    private boolean showingFilter = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.explore_fragment, container, false);
 
-        setFragment(new ListFragment());
+        setFragment(R.id.explore_frag,new ListFragment());
         viewMode = root.findViewById(R.id.view_mode);
         viewMode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 changeViewMode();
+            }
+        });
+
+        FloatingActionButton filterBtn = root.findViewById(R.id.filter);
+
+        filterFragment = new FilterFragment();
+
+        filterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!showingFilter) {
+                    setFragment(R.id.filter_frag, filterFragment);
+                    showingFilter = true;
+                } else{
+                    hideFilter();
+                    showingFilter = false;
+                }
             }
         });
 
@@ -60,20 +80,26 @@ public class ExploreFragment extends Fragment {
             viewMode_Map = false;
 
             listFragment = new ListFragment();
-            setFragment(listFragment);
+            setFragment(R.id.explore_frag,listFragment);
         } else {
             viewMode.setImageResource(R.drawable.ic_baseline_list_24);
             viewMode_Map = true;
 
             mapFragment = new MapFragment();
-            setFragment(mapFragment);
+            setFragment(R.id.explore_frag,mapFragment);
         }
     }
 
-    private void setFragment(Fragment fragment){
+    private void setFragment(int layout,Fragment fragment){
         FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.explore_frag, fragment);
+        transaction.replace(layout, fragment);
         transaction.addToBackStack(null);
+        transaction.commit();
+    }
+    public void hideFilter(){
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+        transaction.remove(filterFragment);
         transaction.commit();
     }
 }
