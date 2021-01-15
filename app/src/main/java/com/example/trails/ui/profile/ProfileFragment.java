76dc0d3f229c5594e974f4739a240f9c2b4c6e42
Renato Vieira <renato.joao9@gmail.com>
2021-01-15 00:32:07 +0000

    Get userid

--- app/src/main/java/com/example/trails/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/trails/ui/profile/ProfileFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,18 +17,26 @@ import com.example.trails.R;
 import com.example.trails.login.LoginActivity;
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileFragment extends Fragment {
 
     private FirebaseAuth mAuth;
 
+    private String userId;
     private ImageButton logout;
+    private TextView username;
 
     private LinearLayout barraEditPerfil;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.profile_fragment, container, false);
+
+        userId  = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        username = root.findViewById(R.id.usernameText);
+        username.setText("teste");
 
         barraEditPerfil = root.findViewById(R.id.barraPerfil);
 
