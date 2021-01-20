38a0ec1d4958cbeac8ef9b9a5be58e5a5b003da8
Renato Vieira <renato.joao9@gmail.com>
2021-01-20 18:43:32 +0000

    Filtro ExploreFragment

--- app/src/main/java/com/example/trails/ui/explore/ExploreFragment.java
+++ app/src/main/java/com/example/trails/ui/explore/ExploreFragment.java
@@ -1,39 +1,62 @@
 package com.example.trails.ui.explore;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.trails.MainActivity;
 import com.example.trails.R;
 import com.example.trails.model.Coordinates;
+import com.example.trails.model.Trail;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.Query;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import static com.example.trails.MainActivity.setFragment;
+import static com.example.trails.controller.DB.db;
 
 public class ExploreFragment extends Fragment {
 
+    private static FilterFragment filterFragment;
+    private static Fragment thisFragment;
     private FloatingActionButton viewMode;
-    private ListFragment listFragment;
+    private static ListFragment listFragment;
     private MapFragment mapFragment;
-    private FilterFragment filterFragment;
-    private View filterLayout;
+    private static View filterLayout;
     public static HashMap<String, Coordinates> trails = new HashMap<>();
 
     private boolean viewMode_Map = false; // true if viewMode is Map, false if viewMode is List.
-    private boolean showingFilter = false;
+    private static boolean showingFilter = false;
+
+    private static FragmentActivity activity;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.explore_fragment, container, false);
+        final View root = inflater.inflate(R.layout.explore_fragment, container, false);
 
-        setFragment(R.id.explore_frag, new ListFragment(), getActivity());
+        thisFragment = this;
+
+        filterFragment = new FilterFragment();
+
+        listFragment = new ListFragment();
+
+        activity = (FragmentActivity)root.getContext();
+
+
+        setFragment(R.id.explore_frag, listFragment, getActivity());
         viewMode = root.findViewById(R.id.view_mode);
         viewMode.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +68,6 @@ public class ExploreFragment extends Fragment {
         final FloatingActionButton filterBtn = root.findViewById(R.id.filter);
 
         filterLayout = root.findViewById(R.id.filter_frag);
-
-        filterFragment = new FilterFragment();
 
         filterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +84,7 @@ public class ExploreFragment extends Fragment {
             }
         });
 
+
         return root;
     }
 
@@ -72,7 +94,6 @@ public class ExploreFragment extends Fragment {
             viewMode.setImageResource(R.drawable.ic_baseline_map_24);
             viewMode_Map = false;
 
-            listFragment = new ListFragment();
             setFragment(R.id.explore_frag, listFragment, getActivity());
         } else {
             viewMode.setImageResource(R.drawable.ic_baseline_list_24);
@@ -83,10 +104,66 @@ public class ExploreFragment extends Fragment {
         }
     }
 
-    public void hideFilter() {
-        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+    public static void hideFilter() {
+        FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
 
         transaction.remove(filterFragment);
         transaction.commit();
+    }
+
+    public static void filterData(boolean terrainEasy, boolean terrainMedium, boolean terrainHard, boolean easy, boolean medium, boolean hard, float rating) {
+        hideFilter();
+        filterLayout.setClickable(false);
+        showingFilter = false;
+
+
+
+        Query query = db.collection("trails");
+
+//        query = query.whereGreaterThanOrEqualTo("characteristics.distance", distanceLow);
+//        query = query.whereLessThanOrEqualTo("characteristics.distance", distanceHigh);
+
+        List<String> terrenoFields = new ArrayList<String>();
+
+        if (terrainEasy) {
+            terrenoFields.add("Baixo");
+        }
+        if (terrainMedium) {
+            terrenoFields.add("Medio");
+        }
+        if (terrainHard) {
+            terrenoFields.add("Alto");
+        }
+
+        if (terrainEasy || terrainMedium || terrainHard) {
+            query = query.whereIn("characteristics.terrainType", terrenoFields);
+        }
+
+        List<String> fields = new ArrayList<String>();
+
+        if (easy) {
+            fields.add("Facil");
+        }
+        if (medium) {
+            fields.add("Mediano");
+        }
+        if (hard) {
+            fields.add("Dificil");
+        }
+
+        if (easy || medium || hard) {
+            query = query.whereIn("characteristics.difficulty", fields);
+        }
+
+        if (rating != 0) {
+            query = query.whereGreaterThanOrEqualTo("trailRating", rating);
+        }
+
+        FirestoreRecyclerOptions<Trail> options = new FirestoreRecyclerOptions.Builder<Trail>().setQuery(query, Trail.class).build();
+
+        listFragment.setQuery(query);
+
+
+        return;
     }
 }
--- app/src/main/java/com/example/trails/ui/explore/FilterFragment.java
+++ app/src/main/java/com/example/trails/ui/explore/FilterFragment.java
@@ -13,42 +13,55 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.trails.R;
+import com.example.trails.model.Trail;
+import com.example.trails.model.User;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.slider.LabelFormatter;
 import com.google.android.material.slider.RangeSlider;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.List;
+
+import static com.example.trails.controller.DB.db;
 public class FilterFragment extends Fragment {
 
     private Button btnShow;
     private Button btnReset;
-    private RadioGroup radioGroup;
-    private RangeSlider rangeSlider;
+    private CheckBox checkBoxTerrainEasy;
+    private CheckBox checkBoxTerrainMedium;
+    private CheckBox checkBoxTerrainHard;
     private CheckBox checkBoxEasy;
     private CheckBox checkBoxMedium;
     private CheckBox checkBoxHard;
     private RatingBar ratingBar;
 
+    private FirebaseFirestore fireStore;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.filter_fragment, container, false);
 
-        rangeSlider = root.findViewById(R.id.filter_trail_distance_slider);
         btnShow = root.findViewById(R.id.filter_show);
         btnReset = root.findViewById(R.id.filter_reset);
-        radioGroup = root.findViewById(R.id.filter_trail_type);
+        checkBoxTerrainEasy = root.findViewById(R.id.filter_terrain_easy);
+        checkBoxTerrainMedium = root.findViewById(R.id.filter_terrain_medium);
+        checkBoxTerrainHard = root.findViewById(R.id.filter_terrain_hard);
         checkBoxEasy = root.findViewById(R.id.filter_dificulty_easy);
         checkBoxMedium = root.findViewById(R.id.filter_dificulty_medium);
         checkBoxHard = root.findViewById(R.id.filter_dificulty_hard);
         ratingBar = root.findViewById(R.id.filter_trail_rating);
 
+        fireStore = FirebaseFirestore.getInstance();
 
-        rangeSlider.setLabelFormatter(new LabelFormatter() {
-            @NonNull
-            @Override
-            public String getFormattedValue(float value) {
-                return value + " km";
-            }
-        });
-        rangeSlider.setValues(1.0f, 5.0f);
+        //Task<QuerySnapshot> trailMaiorDocumentSnap = fireStore.collection("trails").orderBy("characteristics.distance", Query.Direction.DESCENDING).limit(1).get();
+        //Trail trailMaiorDistancia = trailMaiorDocumentSnap.getResult().getDocuments().get(0).toObject(Trail.class);
+
+        //Task<QuerySnapshot> trailMenorDocumentSnap = fireStore.collection("trails").orderBy("characteristics.distance", Query.Direction.ASCENDING).limit(1).get();
+        //Trail trailMenorDistancia = trailMenorDocumentSnap.getResult().getDocuments().get(0).toObject(Trail.class);
+
 
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,14 +74,23 @@ public class FilterFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
+                boolean terrainEasy = checkBoxTerrainEasy.isChecked();
+                boolean terrainMedium = checkBoxTerrainMedium.isChecked();
+                boolean terrainHard = checkBoxTerrainHard.isChecked();
+
+                boolean easy = checkBoxEasy.isChecked();
+                boolean medium = checkBoxMedium.isChecked();
+                boolean hard = checkBoxHard.isChecked();
+
+                float rating = ratingBar.getRating();
+
+                ExploreFragment.filterData(terrainEasy, terrainMedium, terrainHard, easy, medium, hard, rating);
             }
         });
         return root;
     }
 
     private void resetFilter() {
-        radioGroup.clearCheck();
-        rangeSlider.setValues(1.0f, 5.0f);
         ratingBar.setRating((float) 0.0);
 
         if (checkBoxEasy.isChecked()) {
@@ -81,4 +103,6 @@ public class FilterFragment extends Fragment {
             checkBoxHard.toggle();
         }
     }
+
+
 }
