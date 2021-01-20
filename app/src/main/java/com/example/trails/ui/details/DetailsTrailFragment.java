385750ae722578b65fb28ec512aafa19f19fa5b9
JoaoLopes97 <joaodiogolopes97@hotmail.com>
2021-01-20 16:06:07 +0000

    added the reviews to the detail of a trail

--- app/src/main/java/com/example/trails/ui/details/DetailsTrailFragment.java
+++ app/src/main/java/com/example/trails/ui/details/DetailsTrailFragment.java
@@ -1,5 +1,6 @@
 package com.example.trails.ui.details;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -15,6 +17,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.trails.R;
 import com.example.trails.controller.DB;
@@ -23,18 +27,24 @@ import com.example.trails.model.Characteristics;
 import com.example.trails.model.SingletonCurrentUser;
 import com.example.trails.model.Trail;
 import com.example.trails.model.User;
+import com.example.trails.ui.explore.ExploreTrailAdapter;
 import com.example.trails.ui.explore.MapFragment;
 import com.example.trails.ui.start.StartFragment;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.Query;
 
 import static com.example.trails.MainActivity.setFragment;
+import static com.example.trails.controller.DB.db;
 
 public class DetailsTrailFragment extends Fragment {
 
     private TextView titleWalk, description, distance, time_spent, location, terrain, difficulty;
-    private LinearLayout commentsTitle, commentsContent, photosTitle, photosContent;
+    private LinearLayout commentsTitle, photosTitle, photosContent;
+    private RecyclerView commentsRecyclerView;
     private RatingBar ratingBar;
     private ImageButton downloadWalk;
     private Button startWalk;
+    private ImageView commentsButton,photosButton;
     public Trail trail;
     private ImageFlipperFragment imageFlipper;
     private CheckBox favoriteCheckBox;
@@ -66,18 +76,22 @@ public class DetailsTrailFragment extends Fragment {
         setFragment(R.id.map_fragment, mapDraw, getActivity());
 
         commentsTitle = root.findViewById(R.id.commentsTitle);
-        commentsContent = root.findViewById(R.id.commentsContent);
+        commentsButton = root.findViewById(R.id.comment_button);
+        commentsRecyclerView = root.findViewById(R.id.comments_recycler_view);
 
+        photosButton = root.findViewById(R.id.photo_button);
         photosTitle = root.findViewById(R.id.photosTitle);
         photosContent = root.findViewById(R.id.photosContent);
 
         commentsTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (commentsContent.getVisibility() == View.GONE) {
-                    commentsContent.setVisibility(View.VISIBLE);
+                if (commentsRecyclerView.getVisibility() == View.GONE) {
+                    commentsRecyclerView.setVisibility(View.VISIBLE);
+                    commentsButton.setImageResource(R.drawable.ic_baseline_keyboard_arrow_right_24);
                 } else {
-                    commentsContent.setVisibility(View.GONE);
+                    commentsRecyclerView.setVisibility(View.GONE);
+                    commentsButton.setImageResource(R.drawable.down);
                 }
             }
         });
@@ -87,14 +101,23 @@ public class DetailsTrailFragment extends Fragment {
             public void onClick(View v) {
                 if (photosContent.getVisibility() == View.GONE) {
                     photosContent.setVisibility(View.VISIBLE);
+                    photosButton.setImageResource(R.drawable.ic_baseline_keyboard_arrow_right_24);
                 } else {
                     photosContent.setVisibility(View.GONE);
+                    photosButton.setImageResource(R.drawable.down);
                 }
             }
         });
 
         fillFragment();
 
+        commentsRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        commentsRecyclerView.setLayoutManager(mLayoutManager);
+        ReviewTrailAdapter mAdapter = new ReviewTrailAdapter(requireContext(), requireActivity(),trail.getListReviews());
+        commentsRecyclerView.setAdapter(mAdapter);
+
+        commentsRecyclerView.setNestedScrollingEnabled(false);
         setFragment(R.id.images_frag, new ImageFlipperFragment(trail.getImages(), trail.getImagesCoords()), getActivity());
 
         startWalk.setOnClickListener(new View.OnClickListener() {
@@ -135,13 +158,13 @@ public class DetailsTrailFragment extends Fragment {
     private void fillFragment() {
         Characteristics trailCh = trail.getCharacteristics();
         titleWalk.setText(trailCh.getName());
-        //ratingBar.setNumStars(5); //change when comments are added
+        ratingBar.setRating(trail.getTrailRating());
         description.setText(trailCh.getDescription());
         location.setText(trailCh.getLocation().getAddress());
         terrain.setText(trailCh.getTerrainType().name());
         difficulty.setText(trailCh.getDifficulty().name());
-        distance.setText(trailCh.getDistance() + " km"); //use resource string with placeholders
-        time_spent.setText(trailCh.getTimeSpent() + "seconds");
+        distance.setText(trailCh.getDistance() + " km");
+        time_spent.setText(trailCh.getTimeSpent() + " seconds");
 
         if(isFavoriteForUser()){
             favoriteCheckBox.setChecked(true);
---
+++ app/src/main/java/com/example/trails/ui/details/ReviewTrailAdapter.java
@@ -0,0 +1,84 @@
+package com.example.trails.ui.details;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.trails.R;
+import com.example.trails.controller.DB;
+import com.example.trails.controller.LocalDB;
+import com.example.trails.model.Characteristics;
+import com.example.trails.model.Review;
+import com.example.trails.model.SingletonCurrentUser;
+import com.example.trails.model.Trail;
+import com.example.trails.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReviewTrailAdapter extends RecyclerView.Adapter<com.example.trails.ui.details.ReviewTrailAdapter.ViewHolder> {
+
+    List<Review> reviews;
+    Context context;
+    Activity activity;
+
+    public ReviewTrailAdapter(Context context, Activity activity, List<Review> reviews) {
+        this.context = context;
+        this.activity = activity;
+        this.reviews = reviews;
+    }
+
+    @NonNull
+    @Override
+    public ReviewTrailAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.review_card_view, parent, false);
+
+        return new ReviewTrailAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ReviewTrailAdapter.ViewHolder holder, int position) {
+        Review review = reviews.get(position);
+        holder.setDetails(review);
+    }
+
+    @Override
+    public int getItemCount() {
+        return reviews.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView userNameCard, reviewCommentCard;
+        RatingBar ratingBarCard;
+        ImageView reviewPhotoCard;
+
+        public ViewHolder(@NonNull final View itemView) {
+            super(itemView);
+
+            userNameCard = itemView.findViewById(R.id.name_user);
+            reviewCommentCard = itemView.findViewById(R.id.review_comment);
+            ratingBarCard = itemView.findViewById(R.id.review_rating);
+            reviewPhotoCard = itemView.findViewById(R.id.user_image);
+        }
+
+        void setDetails(Review review) {
+
+            User user = SingletonCurrentUser.getCurrentUserInstance();
+            userNameCard.setText(user.getName());
+            reviewCommentCard.setText(review.getComment());
+            ratingBarCard.setRating(review.getRating());
+
+            DB.loadWithGlide(context, user.getPhoto(), reviewPhotoCard);
+        }
+    }
+}
