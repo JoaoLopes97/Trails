234da35e59d6517d59b57b4f791dca67dd4dcb84
barbara <barb.97@hotmail.com>
2021-01-21 01:34:50 +0000

    Cards Layout

--- app/src/main/java/com/example/trails/ui/explore/ExploreTrailAdapter.java
+++ app/src/main/java/com/example/trails/ui/explore/ExploreTrailAdapter.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.trails.MainActivity;
